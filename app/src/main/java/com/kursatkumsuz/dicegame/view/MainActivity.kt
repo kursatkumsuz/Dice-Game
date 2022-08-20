c3fcf479d9a7478d5da3ef29e65b5693d49df9a5
DrHistory <kursatkumsuz@gmail.com>
2022-08-20 18:19:25 +0300

    move to view

--- app/src/main/java/com/kursatkumsuz/dicegame/view/MainActivity.kt
+++ app/src/main/java/com/kursatkumsuz/dicegame/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kursatkumsuz.dicegame
+package com.kursatkumsuz.dicegame.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kursatkumsuz.dicegame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
